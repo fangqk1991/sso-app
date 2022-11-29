6169d3654c9d0e60b69e118120287ea8f01cb509
Fang <me@fangqk.com>
2022-11-29 22:26:45 +0800

    (sso-app): 升级依赖模块

--- packages/auth-frontend/src/app.ts
+++ packages/auth-frontend/src/app.ts
@@ -1,9 +1,10 @@
 import { BasicApp } from '@fangcha/vue/app'
-import { AuthPluginForServer } from '@fangcha/vue/auth'
+import { AuthPluginForServer, MySession } from '@fangcha/vue/auth'
 
 const app = new BasicApp({
   appName: 'Fangcha SSO',
   plugins: [AuthPluginForServer()],
+  session: MySession,
   routes: [
     {
       path: '/',
