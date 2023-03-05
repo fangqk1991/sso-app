ba65d2e9f960686265596fbed6dd27ee5424a6b3
Fang <me@fangqk.com>
2023-03-05 17:58:31 +0800

    (admin-react): support email unlink

--- packages/admin-react/src/account/AccountListView.tsx
+++ packages/admin-react/src/account/AccountListView.tsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react'
 import { MyRequest } from '@fangcha/auth-react'
-import { Button, Divider, message, Modal, Space } from 'antd'
-import { Admin_AccountApis, Admin_SsoClientApis } from '@web/sso-common/admin-api'
+import { Button, Divider, message, Space } from 'antd'
+import { Admin_AccountApis } from '@web/sso-common/admin-api'
 import { TableView } from '../core/TableView'
-import { SsoClientModel } from '@fangcha/sso-models'
 import { PageResult } from '@fangcha/tools'
 import { AccountFormDialog } from './AccountFormDialog'
 import { CarrierType, FullAccountModel } from '@fangcha/account-models'
 import { SimpleInputDialog } from '../core/SimpleInputDialog'
 import { CommonAPI } from '@fangcha/app-request'
+import { ConfirmDialog } from '../core/ConfirmDialog'
 
 export const AccountListView: React.FC = () => {
   const [version, setVersion] = useState(0)
@@ -36,6 +36,23 @@ export const AccountListView: React.FC = () => {
             render: (item: FullAccountModel) => (
               <>
                 <b>{item.email}</b>
+                {item.email && (
+                  <ConfirmDialog
+                    title='请确认'
+                    content={`确定解绑此账号 Email[${item.email}] 吗`}
+                    alertType='error'
+                    onSubmit={async () => {
+                      const request = MyRequest(
+                        new CommonAPI(Admin_AccountApis.AccountCarrierUnlink, item.accountUid, CarrierType.Email)
+                      )
+                      await request.quickSend()
+                      message.success('解绑成功')
+                      setVersion(version + 1)
+                    }}
+                    trigger={<Button type='link'>解绑</Button>}
+                  />
+                )}
+
                 <br />
                 <span>{item.accountUid}</span>
               </>
@@ -55,7 +72,7 @@ export const AccountListView: React.FC = () => {
             title: 'Action',
             key: 'action',
             render: (item: FullAccountModel) => (
-              <Space size='middle'>
+              <Space size='small'>
                 <SimpleInputDialog
                   title='输入新邮箱'
                   content={item.email}
