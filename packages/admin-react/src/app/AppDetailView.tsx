5fe07c77f898ceec301ffd2b2bc0277b0b8cc691
Fang <me@fangqk.com>
2023-03-05 22:14:02 +0800

    (admin-react): +TextPreviewDialog

--- packages/admin-react/src/app/AppDetailView.tsx
+++ packages/admin-react/src/app/AppDetailView.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { MyRequest } from '@fangcha/auth-react'
-import { Breadcrumb, Button, Descriptions, Divider, message, Spin, Tag } from 'antd'
+import { Breadcrumb, Button, Descriptions, Divider, message, Spin, Tabs, Tag } from 'antd'
 import { Link, useParams } from 'react-router-dom'
 import { CommonAPI } from '@fangcha/app-request'
 import { AppTypeDescriptor, P_AppInfo } from '@fangcha/account-models'
 import { CommonAppApis } from '@web/sso-common/core-api'
 import { AppFormDialog } from './AppFormDialog'
+import { TextPreviewDialog } from '../core/TextPreviewDialog'
 
 export const AppDetailView: React.FC = () => {
   const { appid = '' } = useParams()
@@ -32,40 +33,63 @@ export const AppDetailView: React.FC = () => {
         <Breadcrumb.Item>{appInfo.name}</Breadcrumb.Item>
       </Breadcrumb>
       <Divider />
-      <AppFormDialog
-        title='编辑信息'
-        params={appInfo}
-        forEditing={true}
-        onSubmit={async (params) => {
-          const request = MyRequest(new CommonAPI(CommonAppApis.AppUpdate, appInfo.appid))
-          request.setBodyData(params)
-          await request.quickSend()
-          message.success('编辑成功')
-          setVersion(version + 1)
-        }}
-        trigger={<Button type='primary'>编辑</Button>}
+
+      <Tabs
+        defaultActiveKey='basic-info'
+        type='card'
+        items={[
+          {
+            label: `基本信息`,
+            key: 'basic-info',
+            children: (
+              <>
+                <AppFormDialog
+                  title='编辑信息'
+                  params={appInfo}
+                  forEditing={true}
+                  onSubmit={async (params) => {
+                    const request = MyRequest(new CommonAPI(CommonAppApis.AppUpdate, appInfo.appid))
+                    request.setBodyData(params)
+                    await request.quickSend()
+                    message.success('编辑成功')
+                    setVersion(version + 1)
+                  }}
+                  trigger={<Button type='primary'>编辑</Button>}
+                />
+                <Divider />
+                <Descriptions title='基本信息'>
+                  <Descriptions.Item label='Appid'>{appInfo.appid}</Descriptions.Item>
+                  <Descriptions.Item label='应用类型'>{AppTypeDescriptor.describe(appInfo.appType)}</Descriptions.Item>
+                  <Descriptions.Item label='应用名'>{appInfo.name}</Descriptions.Item>
+                  <Descriptions.Item label='备注'>{appInfo.remarks}</Descriptions.Item>
+                  <Descriptions.Item label='创建者'>{appInfo.author}</Descriptions.Item>
+                  <Descriptions.Item label='创建时间'>{appInfo.createTime}</Descriptions.Item>
+                  <Descriptions.Item label='更新时间'>{appInfo.updateTime}</Descriptions.Item>
+                  <Descriptions.Item label='管理员'>
+                    <div>
+                      {appInfo.powerUserList.map((email) => {
+                        return (
+                          <Tag color='geekblue' key={email}>
+                            {email}
+                          </Tag>
+                        )
+                      })}
+                    </div>
+                  </Descriptions.Item>
+                </Descriptions>
+                <Divider />
+                <TextPreviewDialog
+                  loadData={async () => {
+                    const request = MyRequest(new CommonAPI(CommonAppApis.AppOpenInfoPreview, appInfo.appid))
+                    return await request.quickSend()
+                  }}
+                  trigger={<Button type='primary'>预览数据</Button>}
+                />
+              </>
+            ),
+          },
+        ]}
       />
-      <Divider />
-      <Descriptions title='基本信息'>
-        <Descriptions.Item label='Appid'>{appInfo.appid}</Descriptions.Item>
-        <Descriptions.Item label='应用类型'>{AppTypeDescriptor.describe(appInfo.appType)}</Descriptions.Item>
-        <Descriptions.Item label='应用名'>{appInfo.name}</Descriptions.Item>
-        <Descriptions.Item label='备注'>{appInfo.remarks}</Descriptions.Item>
-        <Descriptions.Item label='创建者'>{appInfo.author}</Descriptions.Item>
-        <Descriptions.Item label='创建时间'>{appInfo.createTime}</Descriptions.Item>
-        <Descriptions.Item label='更新时间'>{appInfo.updateTime}</Descriptions.Item>
-        <Descriptions.Item label='管理员'>
-          <div>
-            {appInfo.powerUserList.map((email) => {
-              return (
-                <Tag color='geekblue' key={email}>
-                  {email}
-                </Tag>
-              )
-            })}
-          </div>
-        </Descriptions.Item>
-      </Descriptions>
     </div>
   )
 }
