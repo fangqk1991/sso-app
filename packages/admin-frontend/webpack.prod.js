91d38c161579a0c25e8affdb30570d8fc69ae618
Fang <me@fangqk.com>
2023-03-06 09:00:00 +0800

    (admin-frontend): admin-react -> admin-frontend

--- packages/admin-frontend/webpack.prod.js
+++ packages/admin-frontend/webpack.prod.js
@@ -1,8 +1,11 @@
 const { WebpackBuilder } = require('@fangcha/webpack')
 
 module.exports = new WebpackBuilder()
+  .useReact()
   .setDevMode(false)
   .setPublicPath('/')
+  // .setPublicPath(config.TmplDemoDev.cdnURLBase)
+  .setEntry('./src/index.tsx')
   // .setExtras({
   //   optimization: {
   //     minimize: false,
--- packages/admin-frontend/webpack.staging.js
+++ packages/admin-frontend/webpack.staging.js
@@ -1,8 +1,10 @@
 const { WebpackBuilder } = require('@fangcha/webpack')
 
 module.exports = new WebpackBuilder()
+  .useReact()
   .setDevMode(false)
   .setPublicPath('/')
+  .setEntry('./src/index.tsx')
   .setExtras({
     optimization: {
       minimize: false,
