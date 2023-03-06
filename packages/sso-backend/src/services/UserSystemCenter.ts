0164f4f3f5de5b4e08600f835659e7d0ff608b60
Fang <me@fangqk.com>
2023-03-06 09:00:00 +0800

    (sso-backend):  support user-system's initialization

--- packages/sso-backend/src/services/UserSystemCenter.ts
+++ packages/sso-backend/src/services/UserSystemCenter.ts
@@ -1,9 +1,22 @@
 import assert from '@fangcha/assert'
 import { MyPermissionServer } from './MyPermissionServer'
+import { AppType, PermissionHelper } from '@fangcha/account-models'
 
 class _UserSystemCenter {
   public async prepareUserSystemApp() {
-    const app = await MyPermissionServer.findApp('user-system')
+    let app = await MyPermissionServer.findApp('user-system')
+    if (!app) {
+      app = await MyPermissionServer.generateApp({
+        appid: 'user-system',
+        name: 'User System',
+        appType: AppType.Admin,
+        remarks: '',
+        configData: {},
+        permissionMeta: PermissionHelper.defaultPermissionMeta(),
+        powerUserList: [],
+        author: 'user-system@fangcha.net',
+      })
+    }
     assert.ok(!!app, 'user-system 未被创建', 500)
     return app
   }
