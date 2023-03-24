970b09c3bcfe0fb99eb77fcc6098a7ea4b0a4284
Fang <me@fangqk.com>
2023-03-24 21:25:08 +0800

    (account-service): +FeishuServer

---
+++ libraries/account-service/src/FeishuServer.ts
@@ -0,0 +1,60 @@
+import { FCDatabase } from 'fc-sql'
+import { _FeishuDepartment } from './models/feishu/_FeishuDepartment'
+import { _FeishuDepartmentMember } from './models/feishu/_FeishuDepartmentMember'
+import { _FeishuUser } from './models/feishu/_FeishuUser'
+
+interface Options {
+  database: FCDatabase
+
+  // Default: fc_feishu_department
+  tableName_FeishuDepartment?: string
+
+  // Default: fc_feishu_department_member
+  tableName_FeishuDepartmentMember?: string
+
+  // Default: fc_feishu_user
+  tableName_FeishuUser?: string
+}
+
+export class FeishuServer {
+  public readonly options: Options
+  public readonly database: FCDatabase
+  public readonly FeishuDepartment!: { new (): _FeishuDepartment } & typeof _FeishuDepartment
+  public readonly FeishuDepartmentMember!: { new (): _FeishuDepartmentMember } & typeof _FeishuDepartmentMember
+  public readonly FeishuUser!: { new (): _FeishuUser } & typeof _FeishuUser
+
+  private readonly tableName_FeishuDepartment: string
+  private readonly tableName_FeishuDepartmentMember: string
+  private readonly tableName_FeishuUser: string
+
+  constructor(options: Options) {
+    this.options = options
+
+    this.database = options.database
+
+    this.tableName_FeishuDepartment = options.tableName_FeishuDepartment || 'fc_feishu_department'
+    this.tableName_FeishuDepartmentMember = options.tableName_FeishuDepartmentMember || 'fc_feishu_department_member'
+    this.tableName_FeishuUser = options.tableName_FeishuUser || 'fc_feishu_user'
+
+    class FeishuDepartment extends _FeishuDepartment {}
+    FeishuDepartment.addStaticOptions({
+      database: options.database,
+      table: this.tableName_FeishuDepartment,
+    })
+    this.FeishuDepartment = FeishuDepartment
+
+    class FeishuDepartmentMember extends _FeishuDepartmentMember {}
+    FeishuDepartmentMember.addStaticOptions({
+      database: options.database,
+      table: this.tableName_FeishuDepartmentMember,
+    })
+    this.FeishuDepartmentMember = FeishuDepartmentMember
+
+    class FeishuUser extends _FeishuUser {}
+    FeishuUser.addStaticOptions({
+      database: options.database,
+      table: this.tableName_FeishuUser,
+    })
+    this.FeishuUser = FeishuUser
+  }
+}
--- libraries/account-service/src/index.ts
+++ libraries/account-service/src/index.ts
@@ -1,3 +1,4 @@
 export * from './AccountServer'
+export * from './FeishuServer'
 export * from './models/account/_Account'
 export * from './models/account/_AccountCarrier'
