314cd6324c4c542d42f187f13d8dda14d68fe6fb
Fang <me@fangqk.com>
2023-04-11 09:31:02 +0800

    (sso-backend): remove FeishuDepartmentSyncTask bot

--- packages/sso-backend/src/apps/resque/FeishuDepartmentSyncTask.ts
+++ packages/sso-backend/src/apps/resque/FeishuDepartmentSyncTask.ts
@@ -1,10 +1,9 @@
 import { IResqueTask } from '@fangcha/resque'
 import { MyFeishuSync } from '../../services/MyFeishuSync'
-import { _FangchaState } from '@fangcha/backend-kit'
 
 export class FeishuDepartmentSyncTask implements IResqueTask {
   public async perform() {
     await MyFeishuSync.syncRemoteDepartmentsAndUsers()
-    _FangchaState.botProxy.notify(`飞书组织架构已同步`)
+    // _FangchaState.botProxy.notify(`飞书组织架构已同步`)
   }
 }
