370f3b0b180ce61a1af47ebf6eb209e0953986fd
Fang <me@fangqk.com>
2023-12-25 18:04:59 +0800

    (admin-frontend): update dependencies

--- packages/admin-frontend/src/index.tsx
+++ packages/admin-frontend/src/index.tsx
@@ -2,13 +2,12 @@ import ReactDOM from 'react-dom/client'
 import React from 'react'
 import { App } from './App'
 import { FullLaunchContainer } from '@fangcha/auth-react/router'
-import { LaunchContainer } from '@fangcha/auth-react'
 
 const app = ReactDOM.createRoot(document.getElementById('app')!)
 app.render(
   <React.StrictMode>
-    <LaunchContainer allowAnonymous={true}>
+    <FullLaunchContainer allowAnonymous={true}>
       <App />
-    </LaunchContainer>
+    </FullLaunchContainer>
   </React.StrictMode>
 )
