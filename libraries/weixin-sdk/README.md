965437b7ee369f0fb682e212fcd18744a4dc6b3f
Fang <me@fangqk.com>
2024-06-25 16:55:19 +0800

    (weixin-sdk): + WeixinSdkPlugin

---
+++ libraries/weixin-sdk/README.md
@@ -0,0 +1,11 @@
+# weixin-sdk
+
+## Installation
+
+```
+# Use npm
+npm install @fangcha/weixin-sdk
+
+# Or use yarn
+yarn add @fangcha/weixin-sdk
+```
---
+++ libraries/weixin-sdk/src/weixin-sdk/WeixinSdkPlugin.ts
@@ -0,0 +1,23 @@
+import { AppPluginProtocol } from '@fangcha/backend-kit'
+import { _RouterState } from '@fangcha/backend-kit/lib/router'
+import assert from '@fangcha/assert'
+import { WeixinServer } from '@fangcha/account'
+
+export interface WeixinSdkOptions {
+  weixinServer: WeixinServer
+}
+
+export const WeixinSdkPlugin = (options: WeixinSdkOptions): AppPluginProtocol => {
+  return {
+    appWillLoad: () => {
+      assert.ok(!!_RouterState.routerPlugin, 'routerPlugin missing.', 500)
+      const weixinServer = options.weixinServer
+      const routerApp = _RouterState.routerApp
+      routerApp.addMiddlewareBeforeInit(async (ctx, next) => {
+        ctx.weixinServer = weixinServer
+        await next()
+      })
+    },
+    appDidLoad: () => {},
+  }
+}
